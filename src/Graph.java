9c856e5f26b586649a9f38c92a13834117a29a9b
Alexander Sun <technoal9@gmail.com>
2021-12-13 02:28:34 -0500

    completed bfs function

--- src/Graph.java
+++ src/Graph.java
@@ -1,28 +1,39 @@
-import java.util.ArrayList;
+
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class Graph {
-	private final int V; // number of vertices
-	private int E; // number of edges
-	private Set<Integer>[] adj; // adjacency lists
-	public Graph(int V)
-	{
-		this.V = V; this.E = 0;
-		adj = (Set<Integer>[]) new Set[V]; // Create array of lists.
-		for (int v = 0; v < V; v++) // Initialize all lists
-			adj[v] = new HashSet<Integer>(); // to empty.
+	private final int numVertices; // vertices
+	private int numEdges; // edges
+	private Set<Integer>[] adj; // adjacency lists, chose to use sets to prevent double adding an edge as repeats exist in the data
+	// also makes looking if an edge exists a little quicker
+
+	public Graph(int V) { // Initialize empty sets and whatnot
+		this.numVertices = V;
+		this.numEdges = 0;
+		adj = (Set<Integer>[]) new Set[V];
+		for (int v = 0; v < V; v++)
+			adj[v] = new HashSet<Integer>();
 	}
 
-	public int V() { return V; }
-	public int E() { return E; }
-	public void addEdge(int v, int w)
-	{
-		adj[v].add(w); // Add w to v’s list.
-		adj[w].add(v); // Add v to w’s list.
-		E++;
+	public int getNumVertices() {
+		return numVertices;
 	}
-	public Iterable<Integer> adj(int v)
-	{ return adj[v]; }
+
+	public int getNumEdges() {
+		return numEdges;
+	}
+
+	// Add Edge
+	public void addEdge(int v, int w) {
+		// Undirected graph so you can add each vertice to each other's adjacency list
+		adj[v].add(w);
+		adj[w].add(v);
+		numEdges++;
+	}
+
+	// Return Adjacency Set
+	public Set<Integer> adj(int v) {
+		return adj[v];
+	}
 }
--- src/Q1.java
+++ src/Q1.java
@@ -1,34 +1,88 @@
 import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Q1 {
 	public static void main(String[] args) throws IOException, ParseException {
 		SymbolGraph sg = new SymbolGraph("./network_large_directed_multiEdge.json");
+
 		System.out.println(getNeighbors("giCentre", "North America", sg));
+		System.out.println(shortestPath("Lane Harrison", "Robert Gove", sg));
 	}
 
+	// Gets all neighbors with a matching continent
 	public static List<String> getNeighbors(String node, String continent, SymbolGraph sg){
 		List<String> result = new ArrayList<>();
 		Graph graph = sg.graph();
 		Set<Integer> neighbors = (Set<Integer>) graph.adj(sg.index(node));
+
+		// Loops through all neighbors
 		for(int vertice: neighbors){
-			System.out.println(sg.continent(sg.name(vertice)));
+			//System.out.println(sg.continent(sg.name(vertice)));
+
+			// Check Same continent
 			if(sg.continent(sg.name(vertice)).equals(continent)){
 				result.add(sg.name(vertice));
 			}
 		}
+
 		return result;
 	}
 
+	// Runs a BFS until the target is found, then returns the path from start to end
 	public static List<String> shortestPath(String start, String end, SymbolGraph sg){
 		List<String> result = new ArrayList<>();
 
+		// Store visited arrays, previous vertice, and a queue
+		boolean[] visited = new boolean[sg.graph().getNumVertices()];
+		int[] prevVertice = new int[sg.graph().getNumVertices()];
+		int target = sg.index(end);
 
-		
+		Queue<Integer> toVisit = new ArrayDeque<>();
+
+		toVisit.add(sg.index(start));
+		visited[sg.index(start)] = true;
+
+		// Run until queue is empty or target is found
+		while(!toVisit.isEmpty()){
+			int current = toVisit.remove(); // get current Node
+
+			if(current == target){ // Stop if target is found
+				break;
+			}
+
+			// Loop through neighbors
+			for(int neighbor: sg.graph().adj(current)){
+
+				// If you haven't visited
+				if(!visited[neighbor]){
+					//System.out.println(sg.name(neighbor));
+					// Set previous node
+					prevVertice[neighbor] = current;
+
+					// Make sure the node will be visited in the future
+					toVisit.add(neighbor);
+
+					// Set the node as already visited here to prevent later occurrences of this same Node from messing with the path
+					visited[neighbor] = true;
+				}
+			}
+			//System.out.println();
+		}
+
+		// Run through the previous vertices until we reach the start
+		int prev = prevVertice[target];
+		result.add(sg.name(sg.index(end)));
+		while(prev != sg.index(start)){
+			result.add(sg.name(prev));
+			prev = prevVertice[prev];
+		}
+		result.add(sg.name(prev));
+
+		// List is backwards as we added starting at the end
+		Collections.reverse(result);
+
 		return result;
 	}
 }
